61ff4c0d3fc2a22a13d7c28ea1c4fe18b00643d9
but212 <wjddlf211@gmail.com>
2025-06-14 19:55:27 +0900

    refactor: 해당하는 문제 기입 및 문제에 맞게 조정, console.log에 어떤 변수인지 확인가능하게 변경

--- week_1/scripts/write-const-var.js
+++ week_1/scripts/write-const-var.js
@@ -1,28 +1,47 @@
-let myName = "석정일";
+// 1. 사용자 이름을 저장할 변수를 선언하고 "자신의 이름"을 할당하세요.
+let userName = "석정일";
+
+// 2. "절대 영도(온도의 최저 한계)인 -273.15°C"를 담을 상수를 선언하세요.
 const ABSOLUTE_ZERO = "-273.15°C";
+
+// 3. "로그인 여부"를 확인하는 불리언 타입 변수을 선언하고 적절한값을 할당하세요.
 let isLoggedIn = false;
+
+// 4. "사용자 나이" 변수를 선언하고 숫자 값을 할당하세요.
 let userAge = 25;
-let productPrice = "39,900원";
+
+// 5. "상품 가격(예: 39,900원)"을 담는 변수를 작성하세요.
+let productPrice;
+
+// 6. 웹 페이지의 "배경색"을 담는 상수를 작성하고 적절한 값을 할당하세요.
 const BACKGROUND_COLOR = "#eeeeee";
+
+// 7. 사용자가 작성한 "댓글 수(예: 12)"를 담는 변수를 선언하고 값을 할당하세요.
 let commentCount = 12;
+
+// 8. "현재 페이지 번호(예: 3)"를 담는 변수를 작성하고 적절한 값을 할당하세요.
 let currentPageNumber = 3;
+
+// 9. "회원 등급('VIP', '골드', '실버' 중 하나)"을 담는 변수를 작성해보세요.
 let membershipLevel = {
   vip: "VIP",
   gold: "골드",
   silver: "실버",
 };
+
+// 10. "버튼이 클릭되었는지 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
 let isButtonClicked = true;
 
-console.log(myName);
-console.log(ABSOLUTE_ZERO);
-console.log(isLoggedIn);
-console.log(userAge);
-console.log(productPrice);
-console.log(BACKGROUND_COLOR);
-console.log(commentCount);
-console.log(currentPageNumber);
-console.log(membershipLevel);
-console.log(membershipLevel.vip);
-console.log(membershipLevel.gold);
-console.log(membershipLevel.silver);
-console.log(isButtonClicked);
+console.log("userName: " + userName);
+console.log("ABSOLUTE_ZERO: " + ABSOLUTE_ZERO);
+console.log("isLoggedIn: " + isLoggedIn);
+console.log("userAge: " + userAge);
+console.log("productPrice: " + productPrice);
+console.log("BACKGROUND_COLOR: " + BACKGROUND_COLOR);
+console.log("commentCount: " + commentCount);
+console.log("currentPageNumber: " + currentPageNumber);
+console.log("membershipLevel: " + membershipLevel);
+console.log("membershipLevel.vip: " + membershipLevel.vip);
+console.log("membershipLevel.gold: " + membershipLevel.gold);
+console.log("membershipLevel.silver: " + membershipLevel.silver);
+console.log("isButtonClicked: " + isButtonClicked);
