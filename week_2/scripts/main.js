2d210466fd6d33fbfedf3d34fd720c468c87e4b0
Jeong-il Suk <wjddlf211@naver.com>
2025-06-27 21:55:07 +0900

    quality: 포매팅 적용

--- week_2/scripts/main.js
+++ week_2/scripts/main.js
@@ -1,26 +1,27 @@
-const accordionItems = document.querySelectorAll('.accordion .item');
-const CLASS_ACTIVE = 'is-active';
+const accordionItems = document.querySelectorAll(".accordion .item");
+const CLASS_ACTIVE = "is-active";
 
 accordionItems.forEach((clickedItem) => {
-  clickedItem.addEventListener('click', () => {
+  clickedItem.addEventListener("click", () => {
     const parentAccordion = clickedItem.parentElement;
     const content = clickedItem.nextElementSibling;
-    const isCurrentlyExpanded = clickedItem.getAttribute('aria-expanded') === 'true';
+    const isCurrentlyExpanded =
+      clickedItem.getAttribute("aria-expanded") === "true";
 
     accordionItems.forEach((item) => {
       if (item !== clickedItem) {
-        item.setAttribute('aria-expanded', 'false');
+        item.setAttribute("aria-expanded", "false");
         item.parentElement.classList.remove(CLASS_ACTIVE);
         item.nextElementSibling.style.maxHeight = null;
       }
     });
 
     if (!isCurrentlyExpanded) {
-      clickedItem.setAttribute('aria-expanded', 'true');
+      clickedItem.setAttribute("aria-expanded", "true");
       parentAccordion.classList.add(CLASS_ACTIVE);
-      content.style.maxHeight = content.scrollHeight + 'px';
+      content.style.maxHeight = content.scrollHeight + "px";
     } else {
-      clickedItem.setAttribute('aria-expanded', 'false');
+      clickedItem.setAttribute("aria-expanded", "false");
       parentAccordion.classList.remove(CLASS_ACTIVE);
       content.style.maxHeight = null;
     }
