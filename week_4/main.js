5be569026fea5da32fd51619b418fc9469367a6c
Jeong-il Suk <wjddlf211@naver.com>
2025-07-07 09:08:18 +0900

    feat: main.js 변경으로 span에 적용이 됨

--- week_4/main.js
+++ week_4/main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const tl = gsap.timeline();
+  const tl = gsap.timeline({
+    // 타임라인에 포함된 트윈 애니메이션 공통 설정
+    defaults: { opacity: 0, y: 20, duration: 0.4 },
+  });
 
-  tl.from(".banner__title", { y: -80 })
-    .from(".banner__description", { opacity: 0, y: 20 }, "<-0.2")
-    .from(".banner__link", { opacity: 0, y: 20 }, "<-0.2")
-    .from(".banner__image", { opacity: 0, y: 20, stagger: 0.2 }, "<-0.2");
+  tl.from(".banner__title", { y: -80, duration: 0.8 })
+    .from(".banner__description", {}, "-=0.3")
+    .from(".banner__link", {}, "-=0.3")
+    .from(".banner__image", { stagger: 0.1 }, "-=0.3");
 });
